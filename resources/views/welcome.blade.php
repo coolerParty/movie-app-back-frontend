f3eaa43ea660cab55bf0cc884e922a2583b4a30a
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-04-26 21:41:50 +0800

    Movie Card Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,144 @@
             <div
                 class="grid grid-cols-2 sm:grid-cols-3 md:grid-cls-4 gap-4 rounded"
             >
-                Movie Cards
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
+                <x-movie-card>
+                    <x-slot name="image">
+                        <div class="aspect-w-2 aspect-h-3">
+                            <img
+                                class="object-cover"
+                                src="https://cdn.pixabay.com/photo/2022/04/21/22/50/animal-7148487_960_720.jpg"
+                                alt=""
+                            />
+                            <div
+                                class="absolute left-0 top-0 h-8 w-12 bg-gray-800 text-blue-400 text-center font-bold"
+                            >
+                                New
+                            </div>
+                        </div>
+                    </x-slot>
+                    <a href="/">
+                        <div
+                            class="dark:text-white font-bold group-hover:text-blue-400"
+                        >
+                            Movie Title
+                        </div>
+                    </a>
+                </x-movie-card>
             </div>
         </section>
         <section
