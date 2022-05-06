2b6282baa97557d8e303f6acbf82135aa8542471
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-05-06 19:09:09 +0800

    Search Modal Added

---
+++ app/Http/Livewire/Search.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Movie;
+use Livewire\Component;
+
+class Search extends Component
+{
+    public $showSearchModal =false;
+    public $search = '';
+    public $searchResults = [];
+
+    public function showSearch()
+    {
+        $this->showSearchModal = true;
+    }
+
+    public function closeSearchModal()
+    {
+        $this->showSearchModal = false;
+    }
+
+    public function updatedsearch()
+    {
+        $this->searchResults = Movie::search('title',$this->search)->get();
+    }
+
+    public function render()
+    {
+        
+        return view('livewire.search');
+    }
+}
